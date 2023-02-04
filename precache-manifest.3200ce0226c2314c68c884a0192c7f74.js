d6d79e714e4ee9bab4e439c9b2fc8e081ce2f2d2
Kenneth Trinh <klt45@cornell.edu>
2023-02-03 21:15:20 -0500

    Updates

---
+++ precache-manifest.3200ce0226c2314c68c884a0192c7f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5739ed844e452b64c332",
+    "url": "/Animal-Health-Hackathon/static/css/main.96014753.chunk.css"
+  },
+  {
+    "revision": "5739ed844e452b64c332",
+    "url": "/Animal-Health-Hackathon/static/js/main.6a17b5f3.chunk.js"
+  },
+  {
+    "revision": "d23821cfe919aa42bc07",
+    "url": "/Animal-Health-Hackathon/static/js/runtime~main.92a10975.js"
+  },
+  {
+    "revision": "6280fdc5dd80d7662394",
+    "url": "/Animal-Health-Hackathon/static/js/2.37380856.chunk.js"
+  },
+  {
+    "revision": "5854eb9be6dd082bd048",
+    "url": "/Animal-Health-Hackathon/static/js/3.524ce66f.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Animal-Health-Hackathon/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7c4e77daa43c4a390bd6fbbb2b0b88d2",
+    "url": "/Animal-Health-Hackathon/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e624bcbc1d750206b6fe6ee1e1ffd221.js"
+  "/Animal-Health-Hackathon/precache-manifest.3200ce0226c2314c68c884a0192c7f74.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Animal-Health-Hackathon/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
