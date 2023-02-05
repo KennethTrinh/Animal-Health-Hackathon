42916f8213ea288788799752309929327a7a18f5
nd297 <nd297@cornell.edu>
2023-02-04 23:04:58 -0500

    Removed some commented out stuff

--- src/Form.js
+++ src/Form.js
@@ -36,10 +36,8 @@ const MultiStepForm = () => {
     }));
     setSteps(currentSteps);
     setStep(() => Math.min(step + 1, lastStepIndex));
-    // setFormState(values);
     if (isLastStep && isPreviousStepsValid && isValid) {
-    //   alert(JSON.stringify(values));
-        setFormState(values);
+      setFormState(values);
     }
   }, [steps, isLastStep, isPreviousStepsValid, step, lastStepIndex]);
   const onPrevClick = React.useCallback(event => {
@@ -51,36 +49,36 @@ const MultiStepForm = () => {
     flexDirection: 'column',
     justifyContent: 'center'
   }}>
-        <Stepper value={step} items={steps} />
-        <Form initialValues={formState} onSubmitClick={onStepSubmit} render={formRenderProps => <div style={{
+    <Stepper value={step} items={steps} />
+    <Form initialValues={formState} onSubmitClick={onStepSubmit} render={formRenderProps => <div style={{
       alignSelf: 'center'
     }}>
-              <FormElement style={{ width: 480 }}>
-                {stepPages[step]}
-                <span style={{ marginTop: '40px'}} className={'k-form-separator'} />
-                <div style={{ justifyContent: 'space-between', alignContent: 'center'}} 
-                className={'k-form-buttons k-button k-button-md k-rounded-md k-button-solid k-button-solid-bases-end'}>
-                  <span style={{alignSelf: 'center'}}>Step {step + 1} of 3</span>
-                  <div> {step !== 0 ? <Button style={{marginRight: '16px'}} onClick={onPrevClick}>
-                                        Previous
-                                    </Button> : undefined}
-                    <Button themeColor={'primary'} disabled={isLastStep ? !isPreviousStepsValid : false} onClick={formRenderProps.onSubmit}>
-                      {isLastStep ? 'Submit' : 'Next'}
-                    </Button>
-                  </div>
-                </div>
-              </FormElement>
-              <InteractiveChart
-                diet={{
-                protein: { types: ["chicken", "beef", "fish"], quantity: [1, 2, 3] },
-                carbs: { types: ["rice", "potatoes", "pasta"], quantity: [1, 2, 3] },
-                fat: { types: ["butter", "oil", "avocado"], quantity: [1, 2, 3] }
-                }}
-                form_data={formState}
-                isLastStep={isLastStep}
-                /> 
-            </div>} />
-      </div>;
+      <FormElement style={{ width: 480 }}>
+        {stepPages[step]}
+        <span style={{ marginTop: '40px' }} className={'k-form-separator'} />
+        <div style={{ justifyContent: 'space-between', alignContent: 'center' }}
+          className={'k-form-buttons k-button k-button-md k-rounded-md k-button-solid k-button-solid-bases-end'}>
+          <span style={{ alignSelf: 'center' }}>Step {step + 1} of 3</span>
+          <div> {step !== 0 ? <Button style={{ marginRight: '16px' }} onClick={onPrevClick}>
+            Previous
+          </Button> : undefined}
+            <Button themeColor={'primary'} disabled={isLastStep ? !isPreviousStepsValid : false} onClick={formRenderProps.onSubmit}>
+              {isLastStep ? 'Submit' : 'Next'}
+            </Button>
+          </div>
+        </div>
+      </FormElement>
+      <InteractiveChart
+        diet={{
+          protein: { types: ["chicken", "beef", "fish"], quantity: [1, 2, 3] },
+          carbs: { types: ["rice", "potatoes", "pasta"], quantity: [1, 2, 3] },
+          fat: { types: ["butter", "oil", "avocado"], quantity: [1, 2, 3] }
+        }}
+        form_data={formState}
+        isLastStep={isLastStep}
+      />
+    </div>} />
+  </div>;
 };
 
 export default MultiStepForm;
--- src/Home.js
+++ src/Home.js
@@ -8,14 +8,6 @@ function Home() {
   return (
     <div className="App">
       <h1 > Bone Appétit ♥ </h1>
-      {/* <InteractiveChart
-        // nutrition={ {protein: 11, carbs: 2, fat: 2} } 
-        diet={{
-          protein: { types: ["chicken", "beef", "fish"], quantity: [1, 2, 3] },
-          carbs: { types: ["rice", "potatoes", "pasta"], quantity: [1, 2, 3] },
-          fat: { types: ["butter", "oil", "avocado"], quantity: [1, 2, 3] }
-        }}
-      /> */}
       <MultiStepForm />
 
     </div>
